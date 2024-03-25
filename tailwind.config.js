1b9300562a42d0a109b23c7be8efa9dd4541fe04
Muhammad Nizar <nizares234@gmail.com>
2024-03-25 18:12:51 +0800

    update dockerfile (?)

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.html",
+  "./static/**/*.{js, css}",
+  ],
+  theme: {
+    colors: {
+      'cs-brown': '#AD8B73',
+      'cs-light-brown': '#CEAB93',
+      'cs-sp-light-brown': '#E3CAA5',
+      'cs-cream': '#FFFBE9',
+      'cs-black': '#222222',
+      'cs-white': '#F5F5F5',
+      'cs-brown-black': '#261202',
+      'cs-brown-whites': '#784F31'
+    },
+    fontFamily: {
+      'sans': ['Poppins', 'sans-serif']
+    },
+    extend: {
+      // colors: {
+      //     'cs-brown': '#AD8B73',
+      //     'cs-light-brown': '#CEAB93',
+      //     'cs-sp-light-brown': '#E3CAA5',
+      //     'cs-cream': '#FFFBE9',
+      // },
+    },
+  },
+  plugins: [],
+}
+
